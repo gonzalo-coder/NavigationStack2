01063481b6fdfabf50d12e3d570c80f095009728
gonzalo <gonzalocastro@mac.com>
2023-09-19 00:43:42 -0400

    Initial Commit

---
+++ NavigationStack2/NavigationStack2App.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStack2App.swift
+//  NavigationStack2
+//
+//  Created by Gonzalo Castro on 9/19/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStack2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
