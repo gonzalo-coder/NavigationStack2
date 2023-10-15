3c300cb6973beb68d78c637e9629174f3a15a11b
Gonzalo Castro <gonzalocastro@mac.com>
2023-10-14 23:34:46 -0400

    use NavigationPath() for a mix of destination types

--- NavigationStack2/ContentView.swift
+++ NavigationStack2/ContentView.swift
@@ -7,41 +7,66 @@
 
 import SwiftUI
 
+struct Movie: Hashable {
+    let title: String
+    let rating: Int
+    let watched: Bool
+}
+
+let movies = [Movie(title: "Toy Story", rating: 5, watched: true),
+              Movie(title: "Monsters Inc", rating: 4, watched: true),
+              Movie(title: "The Incredibles", rating: 5, watched: true),
+              Movie(title: "Cars", rating: 3, watched: true),
+              Movie(title: "Ratatouille", rating: 3, watched: true),
+              Movie(title: "Up", rating: 4, watched: true),
+              Movie(title: "WALL-E", rating: 4, watched: true)]
+
 struct ContentView: View {
-    @State private var path: [Int] = []
+    @State private var path = NavigationPath()
 
     var body: some View {
         NavigationStack(path: $path) {
-            VStack {
-                List {
-                    Section(header: Text("Section A")) {
-                        NavigationLink("One", value: 1)
-                        NavigationLink("Two", value: 2)
-                        NavigationLink("Three", value: 3)
-                    }
-                    Section(header: Text("Section B")) {
-                        NavigationLink("Four", value: 4)
-                        NavigationLink("Five", value: 5)
-                        NavigationLink("Six", value: 6)
-                    }
-                    Button("Go to Five") {
-                        path.append(5)
+            List {
+                Section(header: Text("Section A")) {
+                    NavigationLink("One", value: 1)
+                    NavigationLink("Two", value: 2)
+                    NavigationLink("Three", value: 3)
+                    NavigationLink("Four", value: 4)
+                    NavigationLink("Five", value: 5)
+                    NavigationLink("Six", value: 6)
+                }
+                Section(header: Text("Movies")) {
+                    ForEach(movies, id: \.self) { movie in
+                        NavigationLink(movie.title, value: movie)
                     }
                 }
-                .navigationDestination(for: Int.self, destination: { num in
-                    if num % 2 == 0 {
-                        EvenDetail(path: $path, num: num)
-                    } else {
-                        OddDetail(path: $path, num: num)
+                Button("Random Movie") {
+                    if let r = movies.randomElement() {
+                        path.append(r)
                     }
-                })
+                }
             }
+            .navigationTitle("NavigationStack")
+            .navigationDestination(for: Int.self, destination: { num in
+                if num % 2 == 0 {
+                    EvenDetail(path: $path, num: num)
+                        .navigationTitle("Even Detail")
+                        .navigationBarTitleDisplayMode(.inline)
+                } else {
+                    OddDetail(path: $path, num: num)
+                        .navigationTitle("Odd Detail")
+                        .navigationBarTitleDisplayMode(.inline)
+                }
+            })
+            .navigationDestination(for: Movie.self, destination: { model in
+                MovieDetail(path: $path, model: model)
+            })
         }
     }
 }
 
 struct EvenDetail: View {
-    @Binding var path: [Int]
+    @Binding var path: NavigationPath
     let num: Int
     
     var body: some View {
@@ -52,13 +77,28 @@ struct EvenDetail: View {
 }
 
 struct OddDetail: View {
-    @Binding var path: [Int]
+    @Binding var path: NavigationPath
     let num: Int
     
     var body: some View {
         Button("\(num) is odd") {
             path.removeLast()
         }
+    }
+}
+
+struct MovieDetail: View {
+    @Binding var path: NavigationPath
+    let model: Movie
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Title: \(model.title)")
+            Text("Rating: \(model.rating)")
+            Text("Watched: \(model.watched ? "Yes" : "No")")
+        }
+        .navigationTitle("Movie Detail")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
